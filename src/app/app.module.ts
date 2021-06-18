395716a14e13802e645bd5467710f5cdc9040cb8
Federico Trotta <ftrotta18@gmail.com>
2021-06-18 14:34:48 -0300

    Primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { NuevoComponenteComponent } from './nuevo-componente/nuevo-componente.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, NuevoComponenteComponent, NavBarComponent, FooterComponent],
+  imports: [BrowserModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
