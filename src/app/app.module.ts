bceca434c47c0ef55a54ce49946f37b70391502b
Federico Trotta <ftrotta18@gmail.com>
2021-06-20 22:57:28 -0300

    Router links

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
+import { RouterModule, Routes } from "@angular/router"
+// components
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { FooterComponent } from './footer/footer.component';
@@ -11,9 +12,18 @@ import { AboutMeComponent } from './about-me/about-me.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { EducationComponent } from './education/education.component';
 
+const appRoutes: Routes = [
+{ path: "", component: MainContentComponent } ,
+{ path: "CodeProjects", component: ProjectsComponent } ,
+{ path: "WorkExperience", component: AboutMeComponent } ,
+{ path: "Education", component: EducationComponent } ,
+{ path: "AboutMe", component: AboutMeComponent } ,
+];
+
+
 @NgModule({
   declarations: [AppComponent, NavBarComponent, FooterComponent, MainContentComponent, SideBarComponent, AboutMeComponent, ProjectsComponent, EducationComponent],
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, RouterModule.forRoot(appRoutes)],
   providers: [],
   bootstrap: [AppComponent],
 })
