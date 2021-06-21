08d394b62c4c66692e0453f7d13b27938213cfcf
Federico Trotta <ftrotta18@gmail.com>
2021-06-21 14:25:41 -0300

    Algunos componentes extras

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,18 +11,21 @@ import { SideBarComponent } from './side-bar/side-bar.component';
 import { AboutMeComponent } from './about-me/about-me.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { EducationComponent } from './education/education.component';
+import { WorkExperienceComponent } from './work-experience/work-experience.component';
+import { ContactComponent } from './contact/contact.component';
 
 const appRoutes: Routes = [
-{ path: "", component: MainContentComponent } ,
+{ path: "", component: ProjectsComponent } ,
 { path: "CodeProjects", component: ProjectsComponent } ,
-{ path: "WorkExperience", component: AboutMeComponent } ,
+{ path: "WorkExperience", component: WorkExperienceComponent } ,
 { path: "Education", component: EducationComponent } ,
 { path: "AboutMe", component: AboutMeComponent } ,
+{ path: "Contact", component: ContactComponent } ,
 ];
 
 
 @NgModule({
-  declarations: [AppComponent, NavBarComponent, FooterComponent, MainContentComponent, SideBarComponent, AboutMeComponent, ProjectsComponent, EducationComponent],
+  declarations: [AppComponent, NavBarComponent, FooterComponent, MainContentComponent, SideBarComponent, AboutMeComponent, ProjectsComponent, EducationComponent, WorkExperienceComponent, ContactComponent],
   imports: [BrowserModule, FormsModule, RouterModule.forRoot(appRoutes)],
   providers: [],
   bootstrap: [AppComponent],
